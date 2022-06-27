d5de857ff7a67fed1f49c82c04ca86589d49f51d
Wentong Wu <wuwentong2000@gmail.com>
2022-06-27 21:15:58 +0100

    Just some spacing in Test.py

--- Test.py
+++ Test.py
@@ -2,6 +2,7 @@ from statistics import mode
 import cv2
 from keras.models import load_model
 import numpy as np
+
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
@@ -13,6 +14,7 @@ while True:
     normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
     data[0] = normalized_image
     prediction = model.predict(data)
+
     if prediction[0][0] > 0.5:
         print('Rock')
     elif prediction[0][1] > 0.5:
@@ -21,6 +23,7 @@ while True:
         print('Scissors')
     else:
         print('Nothing')
+    
     cv2.imshow('frame', frame)
     # Press q to close the window
     print(prediction)
